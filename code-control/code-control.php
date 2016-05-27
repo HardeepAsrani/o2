ef7c639faa44016547103d94e29b08681ad61402
HardeepAsrani <hardeepasrani@gmail.com>
2016-05-27 07:29:12 +0530

    Code consistency changes for code-control/code-control.php

--- code-control/code-control.php
+++ code-control/code-control.php
@@ -5,10 +5,10 @@
  * This control adds a Font Awesome icon picker which allows you
  * to easily pick icons from Font Awesome.
  *
- * Code Highlighter is a part of O2 library, which is a 
+ * Code Highlighter is a part of O2 library, which is a
  * free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or 
+ * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @package O2 Customizer Library
  * @subpackage Code Highlighter
  * @since 0.1
@@ -27,7 +27,7 @@ function o2_add_code_control( $wp_customize ) {
 	class O2_Customizer_Code_Control extends WP_Customize_Control {
 
 		public $type = 'code-editor';
-		
+
 		public function to_json() {
 			$this->json['choices'] = $this->choices;
 			parent::to_json();
@@ -87,5 +87,3 @@ function o2_add_code_control( $wp_customize ) {
 	}
 }
 add_action( 'customize_register', 'o2_add_code_control' );
-
-?>
