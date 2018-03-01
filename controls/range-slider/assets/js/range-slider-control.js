84168888714d620174e192c1ef0654230e3b4d6f
Hardeep Asrani <hardeepasrani@gmail.com>
2018-03-02 05:11:14 +0530

    Improved JS handling

--- controls/range-slider/assets/js/range-slider-control.js
+++ controls/range-slider/assets/js/range-slider-control.js
@@ -7,18 +7,15 @@ wp.customize.controlConstructor['o2-range-slider'] = wp.customize.Control.extend
 			slider = control.container.find( '.o2-range-slider-range' ),
 			output = control.container.find( '.o2-range-slider-value' );
 
-		output[0].value = slider[0].value;
-
 		slider[0].oninput = function() {
-			output[0].value = this.value;
+			control.setting.set( this.value );
 		}
 
 		if ( control.params.default !== false ) {
 			var reset = control.container.find( '.o2-range-reset-slider' );
 
 			reset[0].onclick = function() {
-				slider[0].value = control.params.default;
-				output[0].value = control.params.default;
+				control.setting.set( control.params.default );
 			}
 		}
 	}
